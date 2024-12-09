803b89ee1f10c931b7798abcf2f9c71b4df85f1d
samsur <139971389+samsure13003@users.noreply.github.com>
2024-12-09 14:24:47 +0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function clickme(){
-    document.write("It hasn't done yet, please get back to the previous page");
-}
-document.getElementById("clickable").addEventListener("click",function(){
-    window.location.href="cart.html";
-})
